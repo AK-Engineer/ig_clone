28cb8025e055471b6ec6ec5097741e000cdccfe1
Andreas Kastis <andreas.ksts@gmail.com>
2019-06-27 13:57:52 +0300

    Create Photos table

--- ig_clone.sql
+++ ig_clone.sql
@@ -9,7 +9,15 @@ CREATE TABLE users(
 );
 
 --Testing insert
-INSERT INTO users (username) VALUES 
-('Blue'),
-('Charlie'),
-('Colt');
+-- INSERT INTO users (username) VALUES 
+-- ('Blue'),
+-- ('Charlie'),
+-- ('Colt');
+
+CREATE TABLE photos (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR(255) NOT NULL,
+    user_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
