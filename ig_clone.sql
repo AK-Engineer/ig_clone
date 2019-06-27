defbca502e94128a0e361182a27408769b5a8885
Andreas Kastis <andreas.ksts@gmail.com>
2019-06-27 13:41:32 +0300

    Initial Creation of DB and Users table

---
+++ ig_clone.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE ig_clone;
+
+USE ig_clone;
+
+CREATE TABLE users(
+	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	username VARCHAR(255) UNIQUE NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW()
+);
+
+--Testing insert
+INSERT INTO users (username) VALUES 
+('Blue'),
+('Charlie'),
+('Colt');
