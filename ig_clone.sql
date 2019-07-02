ca008f5163f83dc3dda4b48f80481529b8ef6ab0
Andreas Kastis <andreas.ksts@gmail.com>
2019-07-02 15:38:24 +0300

    Practise,bug fixes,data

--- ig_clone.sql
+++ ig_clone.sql
@@ -41,7 +41,7 @@ CREATE TABLE follows(
 	followee_id INTEGER NOT NULL,
 	created_at TIMESTAMP DEFAULT NOW(),
 	FOREIGN KEY (follower_id) REFERENCES users(id),
-	FOREIGN KEY (followee_id) REFERENCES user(id),
+	FOREIGN KEY (followee_id) REFERENCES users(id),
 	PRIMARY KEY (follower_id,followee_id)
 );
 
